d6057d41ce99ae1ca40dd91554e19a71fa4609a0
nicolas <nicolas.sabbatini.95@gmail.com>
2021-09-11 19:42:01 -0300

    install script

---
+++ install.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+echo "#######################"
+echo "#Instaling 🦀 RUST 🦀.#"
+echo "#######################"
+
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+rustup target add wasm32-unknown-unknown
+cargo install cargo-update simple-http-server cargo-watch bat cargo-generate wasm-bindgen-cli
