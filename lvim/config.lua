249a0a66d2129cf20867bb9846973c6c5f80efb7
Nicolas Cesar Sabbatini Vrech <nicolas.sabbatini.95@gmail.com>
2022-03-30 14:21:03 -0300

    LvimUpdate

--- lvim/config.lua
+++ lvim/config.lua
@@ -25,7 +25,7 @@ lvim.keys.normal_mode["<C-s>"] = ":w<cr>"
 
 -- Plugins
 -- After changing plugin config exit and reopen LunarVim, Run :PackerInstall :PackerCompile
-lvim.builtin.dashboard.active = true
+lvim.builtin.alpha.active = true
 lvim.builtin.terminal.active = true
 lvim.builtin.nvimtree.setup.view.side = "left"
 lvim.builtin.nvimtree.show_icons.git = 0
