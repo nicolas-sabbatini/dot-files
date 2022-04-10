7a7f28bfadb26c5112aefd95c3ece6973776e4b0
Nicolas Cesar Sabbatini Vrech <nik.code.things@gmail.com>
2022-04-10 01:26:23 -0300

    Pico8 and telemery remove

--- nvim/.config/nvim/lua/nik/lsp/settings/sumneko_lua.lua
+++ nvim/.config/nvim/lua/nik/lsp/settings/sumneko_lua.lua
@@ -8,8 +8,12 @@ return {
 				library = {
 					[vim.fn.expand("$VIMRUNTIME/lua")] = true,
 					[vim.fn.stdpath("config") .. "/lua"] = true,
-				},
+				  ["nik.lsp.settings.extras.pico8.lua"] = true,
+        },
 			},
+      telemetry = {
+            enable = false,
+      },
     },
   },
 }
