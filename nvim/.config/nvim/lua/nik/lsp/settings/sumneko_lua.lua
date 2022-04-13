d696b1e590974b13a2adca4c5a33b0778c288eec
Nicolas Cesar Sabbatini Vrech <nik.code.things@gmail.com>
2022-04-12 23:11:06 -0300

    Remove pico8

--- nvim/.config/nvim/lua/nik/lsp/settings/sumneko_lua.lua
+++ nvim/.config/nvim/lua/nik/lsp/settings/sumneko_lua.lua
@@ -2,17 +2,16 @@ return {
   settings = {
     Lua = {
       diagnostics = {
-				globals = { "vim" },
-			},
-			workspace = {
-				library = {
-					[vim.fn.expand("$VIMRUNTIME/lua")] = true,
-					[vim.fn.stdpath("config") .. "/lua"] = true,
-				  ["nik.lsp.settings.extras.pico8.lua"] = true,
+        globals = { "vim" },
+      },
+      workspace = {
+        library = {
+          [vim.fn.expand("$VIMRUNTIME/lua")] = true,
+          [vim.fn.stdpath("config") .. "/lua"] = true,
         },
-			},
+      },
       telemetry = {
-            enable = false,
+        enable = false,
       },
     },
   },
