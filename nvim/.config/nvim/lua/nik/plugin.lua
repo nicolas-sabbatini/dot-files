98cafb4ee797118681a3e7af36e580ca815368f4
Nicolas Cesar Sabbatini Vrech <nik.code.things@gmail.com>
2022-04-26 18:40:42 -0300

    cappuchin

--- nvim/.config/nvim/lua/nik/plugin.lua
+++ nvim/.config/nvim/lua/nik/plugin.lua
@@ -47,7 +47,8 @@ return packer.startup(function(use)
   use "nvim-lua/popup.nvim" -- An implementation of the Popup API from vim in Neovim
   use "nvim-lua/plenary.nvim" -- Useful lua functions used by lots of plugins
   -- Colorscheme
-  use "lunarvim/darkplus.nvim"
+  -- use "lunarvim/darkplus.nvim"
+  use "catppuccin/nvim"
   -- Cmp plugins
   use "hrsh7th/nvim-cmp" -- The completion plugin
   use "hrsh7th/cmp-buffer" -- Buffer completions
