9947507db3208596127e6ed8b2e2db0dbd8830c1
Nico Sabbatini <nicolas.sabbatini.vrech@modo.com.ar>
2023-03-28 15:02:17 -0300

    Treesitter plugins

---
+++ nvim/.config/nvim/lua/plugins/treesitter.lua
@@ -0,0 +1,15 @@
+return {
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = {
+      rainbow = {
+        enable = true,
+        extended_mode = true,
+        max_file_lines = nil,
+      },
+    },
+  },
+  {
+    "mrjones2014/nvim-ts-rainbow",
+  },
+}
