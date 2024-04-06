a844fffdbc965f0ec412e34ec30f0d856bdd7721
Nicolas Sabbatini <nicolas.sabbatini.95@gmail.com>
2024-04-05 23:57:45 -0300

    Don't load nagick if the magick rock is not installed

--- nvim/.config/nvim/lua/plugins/image.lua
+++ nvim/.config/nvim/lua/plugins/image.lua
@@ -1,3 +1,5 @@
+local has_image_magick, _ = pcall(require, "magick")
+
 return {
 	"3rd/image.nvim",
 	opts = {
@@ -8,12 +10,13 @@ return {
 				clear_in_insert_mode = false,
 				download_remote_images = true,
 				only_render_image_at_cursor = false,
-				filetypes = { "markdown", "vimwiki", "md" }
-			}
+				filetypes = { "markdown", "vimwiki", "md" },
+			},
 		},
-		hijack_file_patterns = { "*.png", "*.jpg", "*.jpeg", "*.gif", "*.webp" }
+		hijack_file_patterns = { "*.png", "*.jpg", "*.jpeg", "*.gif", "*.webp" },
 	},
 	config = function(_, opts)
 		require("image").setup(opts)
-	end
+	end,
+	cond = has_image_magick,
 }
