640af7f198d9d0c40010e8cb61c21be55eeabfba
Nicolas Sabbatini <nicolas.sabbatini.95@gmail.com>
2025-05-06 20:28:03 -0300

    Arrow

---
+++ nvim/.config/nvim/lua/plugins/arrow.lua
@@ -0,0 +1,26 @@
+return {
+	"otavioschwanck/arrow.nvim",
+	dependencies = {
+		{ "echasnovski/mini.icons" },
+	},
+	opts = {
+		show_icons = true,
+		always_show_path = true,
+		leader_key = "f",
+		separate_save_and_remove = true,
+		buffer_leader_key = "ga",
+		global_bookmarks = false,
+		mappings = {
+			edit = "e",
+			delete_mode = "d",
+			clear_all_items = "C",
+			toggle = "s", -- used as save if separate_save_and_remove is true
+			open_vertical = "v",
+			open_horizontal = "h",
+			quit = "q",
+			remove = "x", -- only used if separate_save_and_remove is true
+			next_item = "]",
+			prev_item = "[",
+		},
+	},
+}
