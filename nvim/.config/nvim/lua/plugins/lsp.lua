3d4d540455733a17176abb25fc182c35bd8aec80
Nicolas Sabbatini <nicolas.sabbatini.95@gmail.com>
2025-07-05 01:42:17 -0300

    Server config

--- nvim/.config/nvim/lua/plugins/lsp.lua
+++ nvim/.config/nvim/lua/plugins/lsp.lua
@@ -30,12 +30,57 @@ return {
 			},
 		})
 
-		local lspconfig = require("lspconfig")
-		lspconfig.util.default_config.capabilities = vim.tbl_deep_extend(
-			"force",
-			lspconfig.util.default_config.capabilities,
-			require("cmp_nvim_lsp").default_capabilities()
-		)
+		vim.lsp.config("rust_analyzer", {
+			settings = {
+				["rust-analyzer"] = {
+					cargo = {
+						allFeatures = true,
+						loadOutDirsFromCheck = true,
+						runBuildScripts = true,
+					},
+					check = {
+						command = "clippy",
+						extraArgs = { "--no-deps", "--", "-W", "clippy::pedantic" },
+					},
+				},
+			},
+		})
+		vim.lsp.config("lua_ls", {
+			on_init = function(client)
+				if client.workspace_folders then
+					local path = client.workspace_folders[1].name
+					if
+						path ~= vim.fn.stdpath("config")
+						and (vim.uv.fs_stat(path .. "/.luarc.json") or vim.uv.fs_stat(path .. "/.luarc.jsonc"))
+					then
+						return
+					end
+				end
+				client.config.settings.Lua = vim.tbl_deep_extend("force", client.config.settings.Lua, {
+					runtime = {
+						version = "LuaJIT",
+						path = {
+							"lua/?.lua",
+							"lua/?/init.lua",
+						},
+					},
+					workspace = {
+						checkThirdParty = true,
+						telemetry = { enable = false },
+						library = {
+							vim.env.VIMRUNTIME,
+							"${3rd}/love2d/library",
+							vim.fn.expand("$VIMRUNTIME/lua"),
+							vim.fn.expand("$VIMRUNTIME/lua/vim/lsp"),
+						},
+					},
+				})
+			end,
+			settings = {
+				Lua = {},
+			},
+		})
+
 		lsp.setup()
 		require("luasnip.loaders.from_vscode").lazy_load()
 		local cmp = require("cmp")
--- nvim/.config/nvim/lua/plugins/mason.lua
+++ nvim/.config/nvim/lua/plugins/mason.lua
@@ -55,51 +55,7 @@ return {
 	{
 		"williamboman/mason-lspconfig.nvim",
 		config = function()
-			require("mason-lspconfig").setup({
-				handlers = {
-					function(server_name)
-						require("lspconfig")[server_name].setup({})
-					end,
-					rust_analyzer = function()
-						require("lspconfig").rust_analyzer.setup({
-							settings = {
-								["rust-analyzer"] = {
-									cargo = {
-										allFeatures = true,
-										loadOutDirsFromCheck = true,
-										runBuildScripts = true,
-									},
-									check = {
-										command = "clippy",
-										extraArgs = { "--no-deps", "--", "-W", "clippy::pedantic" },
-									},
-								},
-							},
-						})
-					end,
-					lua_ls = function()
-						require("lspconfig").lua_ls.setup({
-							settings = {
-								Lua = {
-									runtime = {
-										version = "LuaJIT",
-									},
-									workspace = {
-										checkThirdParty = true,
-										telemetry = { enable = false },
-										library = {
-											"${3rd}/love2d/library",
-											vim.fn.expand("$VIMRUNTIME/lua"),
-											vim.fn.expand("$VIMRUNTIME/lua/vim/lsp"),
-										},
-									},
-								},
-								globals = { "vim" },
-							},
-						})
-					end,
-				},
-			})
+			require("mason-lspconfig").setup()
 		end,
 	},
 }
