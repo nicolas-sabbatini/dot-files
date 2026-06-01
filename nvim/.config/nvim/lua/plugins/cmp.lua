f1903c3c45d9570e0dc0f2d7499cb24bce8c9af5
Nicolas Sabbatini <nicolas.sabbatini.95@gmail.com>
2026-06-01 16:17:38 -0300

    Lsp fill

--- nvim/.config/nvim/lua/plugins/cmp.lua
+++ nvim/.config/nvim/lua/plugins/cmp.lua
@@ -11,7 +11,8 @@ return {
 	opts = function()
 		require("luasnip.loaders.from_vscode").lazy_load()
 		local cmp = require("cmp")
-		local cmp_select = { behavior = cmp.SelectBehavior.Select }
+		-- local cmp_select = { behavior = cmp.SelectBehavior.Select }
+		local cmp_select = {}
 		return {
 			sources = {
 				{ name = "nvim_lsp" },
