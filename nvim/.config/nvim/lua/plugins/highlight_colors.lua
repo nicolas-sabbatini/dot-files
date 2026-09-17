3cc31384a1468d3e095e4006de629784f0c7d767
Nicolas Sabbatini <nicolas.sabbatini.95@gmail.com>
2026-09-17 20:07:43 -0300

    colors

---
+++ nvim/.config/nvim/lua/plugins/highlight_colors.lua
@@ -0,0 +1,11 @@
+return {
+	{
+		"brenoprata10/nvim-highlight-colors",
+		event = { "BufReadPost", "BufNewFile" },
+		opts = {
+			render = "background",
+			virtual_symbol = "■",
+			enable_tailwind = true,
+		},
+	},
+}
